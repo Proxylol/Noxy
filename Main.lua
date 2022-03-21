1e224295eca9e8ec642044085caf0d83350e9dc7
Proxy <69701744+Proxylol@users.noreply.github.com>
2022-03-20 23:16:27 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,8 @@
 -- Lifting Simulator
 if game.PlaceId == (3652625463) then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Proxylol/ddd/main/1'),true))()
+
+-- Space Tycoon
+elseif game.PlaceId == (7535149098) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Proxylol/ddd/main/2'),true))()
 end
