cbf78d1b0280c46f707e583e193c938ea956b132
Proxy <69701744+Proxylol@users.noreply.github.com>
2022-04-12 12:41:43 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -5,4 +5,8 @@ loadstring(game:HttpGet(('https://raw.githubusercontent.com/Proxylol/ddd/main/1'
 -- Space Tycoon
 elseif game.PlaceId == (7535149098) then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Proxylol/ddd/main/2'),true))()
+
+-- Blox Hunt
+elseif game.PlaceId == (70005410) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Proxylol/ddd/main/3'),true))()
 end
